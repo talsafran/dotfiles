7c2561ca59962135485dd4dfe51a601f2d89e80c
Tal Safran <tsafran@gmail.com>
2013-11-05 16:57:26 -0800

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+# Case insensitive tab ocompletion
+echo "set completion-ignore-case on" >> /etc/inputrc
+
+# Increase keyboard rate
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
